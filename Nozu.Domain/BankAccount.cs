0872c4d7d1ae0ef8df19fd727ab55c11afe7083e
Jush Daeniel Tuazon <jush@xamun.ai>
2025-04-09 17:15:56 +0800

    [ADD] Add bank account class

---
+++ Nozu.Domain/BankAccount.cs
@@ -0,0 +1,63 @@
+﻿using System;
+namespace Nozu.Domain
+{
+    public class BankAccount
+    {
+        protected double _balance;
+        protected bool _isLocked;
+        protected int _pin;
+
+        public BankAccount(int pin)
+        {
+            _pin = pin;
+        }
+
+        public double GetBalance()
+        {
+            return _balance;
+        }
+
+        public double Deposit(double input)
+        {
+            if (_isLocked) throw new InvalidOperationException("Account is locked");
+            if (input <= 0) throw new InvalidOperationException("Enter a valid amount");
+            if (input >= double.MaxValue) throw new InvalidOperationException("Large amount not allowed");
+            _balance += input;
+            return _balance;
+        }
+
+        public WithdrawDto Withdraw(double input)
+        {
+            if (_isLocked) throw new InvalidOperationException("Account is locked");
+            if (input <= 0) throw new InvalidOperationException("Withdrawal amount should be grater than zero");
+            if (_balance < input) throw new InvalidOperationException("Insufficient balance");
+            _balance -= input;
+            return new WithdrawDto
+            {
+                Amount = input,
+                RemainingBalance = _balance
+            };
+        }
+
+        public bool ChangePin(int oldPin, int newPin, int confirmNewPin)
+        {
+            if (_pin != oldPin) throw new ArgumentException("Old pin is incorrect");
+            if (newPin != confirmNewPin) throw new ArgumentException("New pin does not match");
+            _pin = newPin;
+            return true;
+        }
+
+        public void LockAccount()
+        {
+            _isLocked = true;
+        }
+
+    }
+
+    public class WithdrawDto
+    {
+        public double Amount { get; set; }
+        public double RemainingBalance { get; set; }
+    }
+}
+
---
+++ Nozu.Tests/BankAccountTests.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using Nozu.Domain;
+
+namespace Nozu.Tests
+{
+    public class BankAccountTests
+    {
+        private readonly BankAccount _account;
+
+        public BankAccountTests()
+        {
+            _account = new BankAccount(123456);
+        }
+
+        [Theory]
+        [InlineData(100d)]
+        [InlineData(500d)]
+        [InlineData(1000d)]
+        public void Deposit_ShouldReturnNewBalance_WhenAmountIsGreaterThanZero(double amount)
+        {
+            var initialBalance = _account.GetBalance();
+
+            var result = _account.Deposit(amount);
+
+            Assert.Equal((initialBalance + amount), result);
+        }
+
+        [Theory]
+        [InlineData(0d)]
+        [InlineData(-100d)]
+        public void Deposit_ShouldThrowException_WhenAmountIsLesserThanOrEqualToZero(double amount)
+        {
+            Assert.Throws<InvalidOperationException>(() => _account.Deposit(amount));
+        }
+
+        [Fact]
+        public void Deposit_ShouldThrowException_WhenAmountIsMaximumValue()
+        {
+            var amount = double.MaxValue;
+
+            Assert.Throws<InvalidOperationException>(() => _account.Deposit(amount));
+        }
+
+        [Fact]
+        public void Deposit_ShouldThrowException_WhenAccountIsLocked()
+        {
+            _account.LockAccount();
+
+            Assert.Throws<InvalidOperationException>(() => _account.Deposit(100d));
+        }
+
+        [Theory]
+        [InlineData(2000d, 1000d, 1000d)]
+        [InlineData(5000d, 3500d, 1500d)]
+        public void Withdraw_ShouldReturnAmountAndRemainingBalance_WhenBalanceIsSufficient(double initialBalance, double amount, double remainingBalance)
+        {
+            _account.Deposit(initialBalance);
+            var result = _account.Withdraw(amount);
+
+            Assert.Equal(amount, result.Amount);
+            Assert.Equal(remainingBalance, result.RemainingBalance);
+        }
+
+        [Fact]
+        public void Withdraw_ShouldThrowException_WhenWithdrawalAmountIsZero()
+        {
+            _account.Deposit(100d);
+
+            Assert.Throws<InvalidOperationException>(() => _account.Withdraw(0));
+        }
+
+        [Theory]
+        [InlineData(500d, 1000d)]
+        [InlineData(1000d, 2500d)]
+        public void Withdraw_ShouldThrowException_WhenBalanceIsInsufficient(double initialBalance, double amount)
+        {
+            _account.Deposit(initialBalance);
+
+            Assert.Throws<InvalidOperationException>(() => _account.Withdraw(amount));
+        }
+
+        [Fact]
+        public void Withdraw_ShouldThrowException_WhenAccountIsLocked()
+        {
+            _account.Deposit(100d);
+            _account.LockAccount();
+
+            Assert.Throws<InvalidOperationException>(() => _account.Withdraw(100d));
+        }
+
+        [Fact]
+        public void ChangePin_ShouldThrowException_WhenOldPinIsInvalid()
+        {
+            Assert.Throws<ArgumentException>(() => _account.ChangePin(456789, 456789, 456789));
+        }
+
+        [Fact]
+        public void ChangePin_ShouldThrowException_WhenNewPinAndConfirmPinDoesNotMatch()
+        {
+            Assert.Throws<ArgumentException>(() => _account.ChangePin(456789, 456789, 567890));
+        }
+
+        [Fact]
+        public void ChangePin_ShouldReturnTrue_WhenOldPinAndNewPinAreValid()
+        {
+            var result = _account.ChangePin(123456, 456789, 456789);
+
+            Assert.True(result);
+        }
+    }
+}
+
